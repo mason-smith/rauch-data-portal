eea073dac8b29a74d6874fff757c2268562c3e9d
mason-smith <mason.smith94@outlook.com>
2019-01-24 07:58:58 -0500

    Module Updates

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const bodyParser = require("body-parser");
+
 const app = express();
+
+passport.use(new GoogleStrategy());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
